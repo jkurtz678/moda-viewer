b9146d75b18a23b90cbf6b355d1af7128eb396bd
Jackson Kurtz <jkurtz678@gmail.com>
2022-04-13 20:31:38 -0700

    add goblin test framework

--- viewer/viewer_test.go
+++ viewer/viewer_test.go
@@ -6,26 +6,30 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/alecthomas/assert"
+	"github.com/franela/goblin"
 )
 
 func TestViewer(t *testing.T) {
-	a := assert.New(t)
-	tmpdir := t.TempDir()
-	configPath := filepath.Join(tmpdir, "config_test.json")
+	g := goblin.Goblin(t)
+	g.Describe("Viewer", func() {
+		g.It("Should load document id from config file", func() {
+			tmpdir := t.TempDir()
+			configPath := filepath.Join(tmpdir, "config_test.json")
 
-	testConfig := ViewerConfig{DocumentID: "1"}
-	file, err := json.Marshal(testConfig)
-	a.NoError(err)
+			testConfig := ViewerConfig{DocumentID: "1"}
+			file, err := json.Marshal(testConfig)
+			g.Assert(err).IsNil()
 
-	a.NoError(ioutil.WriteFile(configPath, file, 0644))
+			g.Assert(ioutil.WriteFile(configPath, file, 0644)).IsNil()
 
-	v := Viewer{
-		ConfigFile: configPath,
-	}
+			v := Viewer{
+				ConfigFile: configPath,
+			}
 
-	config, err := v.readConfig()
-	a.NoError(err)
+			config, err := v.readConfig()
+			g.Assert(err).IsNil()
 
-	a.Equal(testConfig.DocumentID, config.DocumentID)
+			g.Assert(config.DocumentID).Equal(testConfig.DocumentID)
+		})
+	})
 }
