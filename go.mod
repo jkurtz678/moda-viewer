1b95c21602c9570389e2b5a2d7ab07a409ebd20d
Jackson Kurtz <jkurtz678@gmail.com>
2022-05-06 15:08:08 -0700

    tweak vlc http int

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module jkurtz678/moda-viewer
 go 1.16
 
 require (
-	cloud.google.com/go/firestore v1.6.1 // indirect
-	firebase.google.com/go v3.13.0+incompatible // indirect
+	cloud.google.com/go/firestore v1.6.1
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/alecthomas/assert v1.0.0 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/franela/goblin v0.0.0-20211003143422-0a4f594942bf // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/nxadm/tail v1.4.8 // indirect
-	google.golang.org/api v0.59.0 // indirect
+	google.golang.org/api v0.59.0
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- viewer/vlc.go
+++ viewer/vlc.go
@@ -1,13 +1,7 @@
 package viewer
 
 import (
-	"fmt"
-	"log"
-	"os"
 	"os/exec"
-	"strings"
-
-	"github.com/nxadm/tail"
 )
 
 type VideoPlayer interface {
@@ -19,7 +13,7 @@ type VLCPlayer struct{}
 func (v *VLCPlayer) playMedia(filepath string, videoStartCallback func(mediaID string)) error {
 	logger.Printf("playMedia() - %s", filepath)
 	//cmd := exec.Command("vlc", filepath, "--fullscreen", "--loop", "--no-video-title", "--no-macosx-fspanel")
-	go func() {
+	/* go func() {
 		err := os.Truncate("vlc.log", 0)
 		if err != nil {
 			log.Fatal(err)
@@ -44,9 +38,9 @@ func (v *VLCPlayer) playMedia(filepath string, videoStartCallback func(mediaID s
 				}
 			}
 		}
-	}()
+	}() */
 	//cmd := exec.Command("vlc", filepath, "--loop", "--no-video-title", "--no-macosx-fspanel", "--file-logging", "--logfile=vlc.txt", "--log-verbose=3")
 	//cmd := exec.Command("vlc", filepath, "--loop", "--intf=http", "--http-port=9090", "--http-password=m0da", "--no-audio", "--no-video-title", "--verbose=2", "--file-logging", "--logfile=vlc.log", "--log-verbose=3", " --http-port 9090")
-	cmd := exec.Command("vlc", filepath, "--loop", "--intf=http", "--http-port=9090", "--http-password=m0da", "--no-video-title")
+	cmd := exec.Command("vlc", filepath, "--loop", "--extraintf=http", "--http-port=9090", "--http-password=m0da", "--no-video-title")
 	return cmd.Run()
 }
