742cd791124f4c1828dde0fb048e20167e36c601
Jackson Kurtz <jkurtz678@gmail.com>
2022-05-29 10:55:59 -0700

    simplify webview

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/franela/goblin v0.0.0-20211003143422-0a4f594942bf
 	github.com/google/go-cmp v0.5.7
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.1
 	google.golang.org/api v0.59.0
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- viewer/plaque_webview.py
+++ viewer/plaque_webview.py
@@ -1,32 +1,11 @@
 import webview
 import sys
-import zmq
-import threading
+import signal
 
-def zmq_listener(window): 
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:5555")
-    try: 
-        while True: 
-            message = socket.recv()
-            new_url = message.decode("utf-8")
-            print("Received request: %s" % new_url, flush=True)
-            try: 
-                window.load_url(new_url)
-                socket.send(b"done")
-                print("successfully loaded url", flush=True)
-            except Exception as e:
-                print("Error loading url %s" % e, flush=True)
-                socket.send(b"fail")
-    except KeyboardInterrupt:
-        print('zeromq interrupted!')
+# allows keybaord interrupts to work
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 # run webview
-
-try: 
-    plaque_url = sys.argv[1]
-    window = webview.create_window('MoDA Plaque', plaque_url)
-    webview.start(zmq_listener, window)
-except KeyboardInterrupt:
-    print('webview interrupted!')
+plaque_url = sys.argv[1]
+webview.create_window('MoDA Plaque', plaque_url)
+webview.start() 
