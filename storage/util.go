4dcd96ef53940b0ce82f2a2e66e7b51a057ce656
Jackson Kurtz <jkurtz678@gmail.com>
2022-06-17 18:34:15 -0700

    add checks for python3 and python commands

--- storage/util.go
+++ storage/util.go
@@ -7,7 +7,6 @@ import (
 
 // FileExists returns true if file exists, false if not found
 func FileExists(filePath string) (bool, error) {
-	logger.Printf("FileExists %s", filePath)
 	_, err := os.Stat(filePath)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
--- webview/python-webview.go
+++ webview/python-webview.go
@@ -15,8 +15,20 @@ type PythonWebview struct {
 
 func (pw *PythonWebview) InitPlaque() {
 	log.Printf("PythonWebview.InitPlaque() - running plaque webview")
-	cmd := exec.Command("python3", "webview/plaque_webview.py", "http://localhost:8080")
+	// check if python3 command exists
+	_, err := exec.LookPath("python3")
+	if err == nil {
+		// python3 command exists, use that to run python
+		cmd := exec.Command("python3", "webview/plaque_webview.py", "http://localhost:8080")
+		log.Fatalf("PythonWebview.InitPlaque() - error %v", cmd.Run())
+		return
+	}
+
+	// python3 does not exist, use python as argument
+	cmd := exec.Command("python", "webview/plaque_webview.py", "http://localhost:8080")
 	log.Fatalf("PythonWebview.InitPlaque() - error %v", cmd.Run())
+	return
+
 }
 
 /* func (pq *PythonWebview) navigateURL(tokenMetaID string) {
